0c50aca1a6ca148460029add272dfe8d0e6fdb86
BeanieMen <jainarjav886@gmail.com>
2025-10-24 00:28:18 +0530

    Add flake-level bootloader module; remove unnecessary x86_64 input

--- flake.nix
+++ flake.nix
@@ -4,16 +4,16 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs"; 
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, flake-utils, ... }:
+  outputs =
+    inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, flake-utils, ... }:
     let
       system = "x86_64-linux";
       overlay-unstable = final: prev: {
@@ -22,58 +22,54 @@
           config.allowUnfree = true;
         };
       };
+      mkHost = host:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
 
-      mkHost = host: nixpkgs.lib.nixosSystem {
-        inherit system;
+          specialArgs = { inherit self inputs host; };
 
-        specialArgs = {
-          inherit self inputs host;
+          modules = [
+            # centralized flake-level bootloader module
+            ./modules/bootloader.nix
+            {
+              nixpkgs = {
+                config.allowUnfree = true;
+                overlays = [ overlay-unstable ];
+              };
+              nix = {
+                settings = {
+                  auto-optimise-store = true;
+                  experimental-features = [ "nix-command" "flakes" ];
+                  max-jobs = "auto";
+                  cores = 0;
+                  substituters = [
+                    "https://cache.nixos.org"
+                    "https://nix-community.cachix.org"
+                  ];
+                  trusted-public-keys = [
+                    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+                  ];
+                  keep-outputs = true;
+                  keep-derivations = true;
+                };
+                gc = {
+                  automatic = true;
+                  dates = "weekly";
+                  options = "--delete-older-than 30d";
+                };
+              };
+            }
+            ./hosts/${host}/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useUserPackages = true;
+
+              home-manager.users.aj = {
+                imports = [ ./hosts/${host}/home-manager/home.nix ];
+              };
+            }
+          ];
         };
-
-        modules = [
-          {
-            nixpkgs = {
-              config.allowUnfree = true;
-              overlays = [ overlay-unstable ];
-            };
-            nix = {
-              settings = {
-                auto-optimise-store = true;
-                experimental-features = [ "nix-command" "flakes" ];
-                max-jobs = "auto";
-                cores = 0;  
-                substituters = [
-                  "https://cache.nixos.org"
-                  "https://nix-community.cachix.org"
-                ];
-                trusted-public-keys = [
-                  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-                  "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-                ];
-                keep-outputs = true;
-                keep-derivations = true;
-              };
-              gc = {
-                automatic = true;
-                dates = "weekly";
-                options = "--delete-older-than 30d";
-              };
-            };
-          }
-          ./hosts/${host}/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useUserPackages = true;
-
-            home-manager.users.aj = {
-              imports = [ ./hosts/${host}/home-manager/home.nix ];
-            };
-          }
-        ];
-      };
-    in {
-      nixosConfigurations = {
-        Beanie = mkHost "Beanie";
-      };
-    };
+    in { nixosConfigurations = { Beanie = mkHost "Beanie"; }; };
 }
